d85a641fc8c6bc4904027c561ff800e178f43460
_ <_@_.com>
2026-01-27 19:15:41 +0900

    Add "Navigate 50 times" button to TestCLIView

--- src/ZYC.Automation.Modules.Mock/UI/TestCLIView.xaml.cs
+++ src/ZYC.Automation.Modules.Mock/UI/TestCLIView.xaml.cs
@@ -1,12 +1,33 @@
-﻿using ZYC.CoreToolkit.Extensions.Autofac.Attributes;
+﻿using System.Windows;
+using ZYC.Automation.Abstractions.Tab;
+using ZYC.CoreToolkit.Extensions.Autofac.Attributes;
 
 namespace ZYC.Automation.Modules.Mock.UI;
 
 [Register]
 public partial class TestCLIView
 {
-    public TestCLIView()
+    public TestCLIView(ITabManager tabManager)
     {
+        TabManager = tabManager;
         InitializeComponent();
+    }
+
+    private ITabManager TabManager { get; }
+
+    private async void OnNavigate50TimesButtonClick(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            for (var i = 0; i < 50; ++i)
+            {
+                await TabManager.NavigateAsync("zyc://cli");
+                await Task.Delay(50);
+            }
+        }
+        catch
+        {
+            //ignore
+        }
     }
 }
